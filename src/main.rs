0b1965365528f49779c72c0ef034ecc5cdd965db
tilpner <till@hoeppner.ws>
2018-12-08 14:26:58 +0100

    Switch to StoreHash representation

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ pub mod derivation;
 pub mod narinfo;
 
 use std::{
-    env, fs,
+    env, fs, str, mem,
     path::Path,
     collections::HashMap
 };
@@ -15,68 +15,89 @@ use number_prefix::{ binary_prefix, Standalone, Prefixed };
 
 use crate::{ derivation::*, narinfo::* };
 
-fn read_drv(path: &str) -> Drv {
+const NIX_HASH_LENGTH: usize = 32;
+
+fn read_drv<P: AsRef<Path>>(path: P) -> Drv {
     let file_content = fs::read(path).expect("Unable to read derivation");
     let (rest, drv) = derivation::drv(&file_content).expect("Unable to parse derivation");
     assert!(rest.is_empty(), "Less than the entire drv was parsed");
     drv
 }
 
-fn gather_closure(name: String, drv: &Drv) -> HashMap<String, Drv> {
-    fn add_to_closure(out: &mut HashMap<String, Drv>, name: String, drv: &Drv) {
-        if out.contains_key(&name) { return; }
-        out.insert(name, drv.clone());
+fn gather_closure(hash: StoreHash, drv: &Drv) -> HashMap<StoreHash, Drv> {
+    fn add_to_closure(out: &mut HashMap<StoreHash, Drv>, hash: StoreHash, drv: &Drv) {
+        if out.contains_key(&hash) { return; }
+        out.insert(hash, drv.clone());
 
         for InputDrv { path, .. } in &drv.input_drvs {
             // check cache to avoid unnecessary IO/parsing
-            let input_drv = if out.contains_key(path) {
-                out[path].clone()
+            let input_drv_hash = StoreHash::from_path(path);
+            let input_drv = if out.contains_key(&input_drv_hash) {
+                out[&input_drv_hash].clone()
             } else {
                 read_drv(&path)
             };
 
-            add_to_closure(out, path.clone(), &input_drv);
+            add_to_closure(out, input_drv_hash, &input_drv);
         }
     }
 
     let mut closure = HashMap::new();
-    add_to_closure(&mut closure, name, &drv);
+    add_to_closure(&mut closure, hash, &drv);
     closure
 }
 
-// This can parse as Path, then slice the filename,
-// or it can assume /nix/store, which is faster
-fn extract_hash_from_store_path(path: String) -> String {
-    // use std::os::unix::ffi::OsStrExt;
+// Nix store hashes are the first 160 bits of a sha256 hash, base32 encoded.
+// That base32 representation could be decoded into a [u32; 5], but then
+// we'd depend on Nix's exact character set and encoding/decoding rules.
+//
+// sizeof [u8; 32] = 256 bit > 160 bit = sizeof [u32; 5]
+//
+// This means our representation is quite a bit less compact than it could be,
+// but with a typical system closure of 10k paths, that's still less than a megabyte,
+// the derivations themselves are much more expensive.
+#[derive(Hash, Clone, PartialEq, Eq)]
+struct StoreHash([u8; NIX_HASH_LENGTH]);
+impl StoreHash {
+    fn from_path<P: AsRef<Path>>(path: P) -> Self {
+        let hash = &path.as_ref()
+            .file_name().expect("Not a file")
+            .to_str().expect("Invalid characters in filename")
+            .as_bytes()[..NIX_HASH_LENGTH];
 
-    // let file_name = path.file_name().unwrap();
-    
-    // slice just the hash
-    path[11..(11+32)].to_owned()
+        let mut arr: [u8; NIX_HASH_LENGTH] = unsafe { mem::uninitialized() };
+        arr.copy_from_slice(hash);
+
+        StoreHash(arr)
+    }
+
+    fn to_str(&self) -> &str {
+        // no need to check for utf8-ness, content can only be created by from_path
+        unsafe { str::from_utf8_unchecked(&self.0) }
+    }
 }
 
-fn fetch_narinfo(closure: HashMap<String, Drv>) -> Vec<Option<NarInfo>> {
+fn fetch_narinfo<I>(outputs: I) -> Vec<Option<NarInfo>>
+    // Runtime::block_on needs Send + 'static
+    where I: Iterator<Item = StoreHash> + Send + 'static
+{
     let client = Client::new();
-
-    let urls =
-        closure.into_iter()
-               .flat_map(|(path, drv)| drv.outputs)
-               .map(|output| output.path)
-               .map(extract_hash_from_store_path)
-               .map(|hash| format!("http://cache.nixos.org/{}.narinfo", hash));
-
-    let narinfos = stream::iter_ok(urls)
-        .map(move |url| {
+    let narinfos = stream::iter_ok(outputs)
+        .map(move |output| {
+            let url = format!("http://cache.nixos.org/{}.narinfo", output.to_str());
             client.get(&url)
                   .send()
                   .and_then(|res| res.into_body().concat2().from_err())
         })
         .buffer_unordered(128)
-        .map(NarInfo::from)
+        .map(|bytes| {
+            let b: &[u8] = bytes.as_ref();
+            NarInfo::from(b)
+        })
         .collect();
 
     let mut runtime = Runtime::new().expect("Unable to initialise tokio runtime");
-    runtime.block_on(narinfos).unwrap_or(Vec::new())
+    runtime.block_on(narinfos).unwrap_or_default()
 }
 
 fn format_bytes(amount: u64) -> String {
@@ -112,20 +133,15 @@ fn print_statistics(narinfos: Vec<Option<NarInfo>>) {
 fn main() {
     let args: Vec<_> = env::args().collect();
 
-    let build_time_closure = gather_closure(args[1].clone(), &read_drv(&args[1]));
-    // println!("done({}), gathered {} paths", args[1], closure.len());
+    let input_path = fs::canonicalize(&args[1]).expect("Unable to canonicalize path argument");
+    let input_hash = StoreHash::from_path(&input_path);
+    let build_time_closure = gather_closure(input_hash, &read_drv(&input_path));
+    println!("done({}), gathered {} paths", args[1], build_time_closure.len());
 
-    let narinfos = fetch_narinfo(build_time_closure);
+    let build_time_outputs = build_time_closure.into_iter()
+        .flat_map(|(_hash, drv)| drv.outputs)
+        .map(|output| StoreHash::from_path(&output.path));
+    let narinfos = fetch_narinfo(build_time_outputs);
 
     print_statistics(narinfos);
-
-    /*
-    let outputs: Vec<_> =
-        closure.iter()
-               .flat_map(|(path, drv)| drv.outputs.iter())
-               .map(|output| &output.path)
-               .collect();
-
-    for o in outputs { println!("{:?}", o) }
-    */
 }
