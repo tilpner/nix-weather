534b5bf2d6560be8f871d0b2df7fab50a6220eea
tilpner <till@hoeppner.ws>
2019-11-24 16:48:20 +0100

    nix: setup nur

--- default.nix
+++ default.nix
@@ -3,11 +3,17 @@ with builtins;
 with pkgs;
 
 let
-  rust = nur.repos.mozilla.rustChannels.beta.rust;
+  nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+    inherit pkgs;
+  };
+
+  rust = nur.repos.mozilla.rustChannelOf {
+    date = "2019-11-07"; channel = "stable";
+  };
 
   buildRustPackage' = rustPlatform.buildRustPackage.override {
-    rustc = rust;
-    cargo = rust;
+    rustc = rust.rust;
+    cargo = rust.rust;
   };
 in buildRustPackage' rec {
   pname = "nix-weather";
